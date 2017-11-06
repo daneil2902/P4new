601a7bc907f01fef6830b1270cdece9eeaf85d3b
taruc <taruc@B21>
2017-11-06 12:18:39 +0800

    Hello World

--- src/p4new/P4new.java
+++ src/p4new/P4new.java
@@ -15,7 +15,7 @@ public class P4new {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello World");    
     }
     
 }
